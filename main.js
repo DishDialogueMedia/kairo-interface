560d09d767dabdb215c4e99356b669af65dc4f24
DishDialogueMedia <dishdialogue.media@gmail.com>
2025-05-04 08:05:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,8 @@ async function fetchKairoResponse(message) {
 
     // Firestore logging
     if (typeof db !== "undefined") {
-      await db.collection("kairo_log").add({
+  await db.collection("kairo_log").add({ ... });
+}
         user: "Ryan Wisnoski",
         message: message,
         response: data.reply,
